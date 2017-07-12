fe04aa1a9c458e72d573d0f076ac5a4006f95111
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-11 23:45:23 -0400

    Done.

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,5 +1,5 @@
 describe('angularjs homepage todo list', function() {
-	it('should add a todo', function() {
+	it('should add a todo to the list', function() {
 		browser.get('https://angularjs.org');
 
 		element(by.model('todoList.todoText')).sendKeys('Writing tests!!');
